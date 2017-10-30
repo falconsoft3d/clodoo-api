92f65f9463c4425783b53a028460002b7e0b84d7
Jhon Quiufeng <jhonq@gmail.com>
2017-10-29 22:19:51 -0400

    Fichero de configuración de la aplicación

---
+++ conf.py
@@ -0,0 +1,11 @@
+global urlApi,commonEndPoint,objectEndPoint,service,database
+
+
+
+
+
+urlApi = "/api/v1/"
+service = 'http://192.168.1.101:8069'
+database = 'odoo'
+commonEndPoint =  service+"/xmlrpc/2/common"
+objectEndPoint =  service+"/xmlrpc/2/object" 
