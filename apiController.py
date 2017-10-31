b2a75f99e160b05f29bf695f076c1f57f36a5a6b
Jhon Quiufeng <jhonq@gmail.com>
2017-10-31 00:24:06 -0400

    Añadidas las API para listar y contar recursos

--- apiController.py
+++ apiController.py
@@ -16,7 +16,23 @@ class ApiController:
         return self.idUser
     
 
-   
+   def listRecord(self,idUser,password,model,parameter,option={}):
+         method = "search"
+         if not self.checkModelAccessMethod(idUser,password,model,"read"):
+            raise ModelNotMethodAvailableError("El modelo '"+model+"' no tiene habilitado el metodo de consulta 'read'.")
+         
+         call = ApiModel(self.api,self.database,idUser,password,model,method,parameter,option)
+         result = call.execute()
+         return result
+    
+    def countRecord(self,idUser,password,model,parameter,option={}):
+         method = "search_count"
+         if not self.checkModelAccessMethod(idUser,password,model,"read"):
+            raise ModelNotMethodAvailableError("El modelo '"+model+"' no tiene habilitado el metodo de consulta 'read'.")
+         
+         call = ApiModel(self.api,self.database,idUser,password,model,method,parameter,option)
+         result = call.execute()
+         return result
 
 class ModelNotMethodAvailableError(Exception):
     def __init__(self, message):       
